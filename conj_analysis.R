835215b06d36234f80e0a616aef7a788526657f8
rawitner <rawitner@gmail.com>
2019-11-22 16:53:32 -0500

    implemented first version of worst offenders

--- conj_analysis.R
+++ conj_analysis.R
@@ -1,30 +1,26 @@
 library(tidyverse)
 library(RColorBrewer)
-#all_conjs = readRDS("all_conjs")
+all_conjs = readRDS("all_conjs")
 derelicts = readRDS("derelicts")
-#file_list = readRDS("file_list")
+derelictDat = readRDS("derelictDatNew")
+file_list = readRDS("file_list")
 mcma_objs = readRDS("mcma_objs")
-today = "22NOV2019"
-
+today = "22NOV2019" # update daily
 path = "conj_data/"
-file_list = list.files(path)
-#file_list_new = list.files(path)
-#file_list_new = file_list_new[!(file_list_new %in% file_list)] # only the new conjunctions
 
-all_conjs = data.frame()
-for (i in 1:length(file_list)) {
-  temp_data = read_csv(paste0(path, file_list[i]))
-  all_conjs = rbind(all_conjs, temp_data) #for each iteration, bind the new data to the building dataset
+# read in new conjunction files
+file_list_new = list.files(path)
+file_list_new = file_list_new[!(file_list_new %in% file_list)] # only the new conjunctions
+
+all_conjs_new = data.frame()
+for (i in 1:length(file_list_new)) {
+  temp_data = read_csv(paste0(path, file_list_new[i]))
+  all_conjs_new = rbind(all_conjs_new, temp_data) #for each iteration, bind the new data to the building dataset
+  clusts = sort(c(all_conjs_new$PrimaryCluster[i], all_conjs_new$SecondaryCluster[i]), decreasing = T)
+  all_conjs_new$clusters[i] = paste0(clusts[1], "-", clusts[2])
 }
 
-# column for cluster combination
-for (r in 1:nrow(all_conjs)) {
-  clusts = sort(c(all_conjs$PrimaryCluster[r], all_conjs$SecondaryCluster[r]), decreasing = T)
-  all_conjs$clusters[r] = paste0(clusts[1], "-", clusts[2])
-}
-
-# column for unique cluster, if a number and LEO or HIGH, only label as the number
-all_conjs =  all_conjs %>%
+all_conjs_new =  all_conjs_new %>%
   mutate(firstClust = gsub("-.*$", "", clusters),
          secondClust = sub(".*?-", "", clusters),
          clusterLab = if_else(firstClust=="LEO" & secondClust=="LEO", "LEO",
@@ -33,8 +29,14 @@ all_conjs =  all_conjs %>%
                                               if_else(firstClust=="HIGH" & secondClust!="HIGH", 
                                                       "HIGH-other", firstClust)))),
          clusterLab = factor(clusterLab,
-           levels = c("615", "775", "850", "975", "1200", "1500", "LEO","LEO-other","HIGH-other"),
-           ordered = T))
+                             levels = c("615", "775", "850", "975", "1200", "1500", "LEO","LEO-other","HIGH-other"),
+                             ordered = T))
+
+# append new conjunctions to previous
+all_conjs = rbind(all_conjs, all_conjs_new)
+saveRDS(all_conjs, "all_conjs")
+file_list = append(file_list, file_list_new)
+saveRDS(file_list, "file_list")
 
 # read in country codes
 country_codes = read_csv("./country_codes.csv", 
@@ -73,6 +75,133 @@ ggplot() +
 ggsave(paste0("output/percentEncountersByCountry_", today, ".jpeg"),device="jpeg")
 
 #####################
+# miss distance vs cumulative count plot
+
+# axis ticks for log scale
+ticks <- 1:10
+ooms <- 10^(0:3) # define orders of magnitudes
+breaks <- as.vector(ticks %o% ooms)
+
+show.labels <- c(T, F, F, F, T, F, T, F, F)
+labels <- as.character(breaks * show.labels)
+labels <- gsub("^0$", "", labels)
+
+all_conjs %>%
+  group_by(clusterLab) %>%
+  arrange(Range) %>%
+  mutate(rowid = 1, cumnum = cumsum(rowid)) %>% 
+  ggplot(aes(x=Range, y = cumnum, color=clusterLab)) + 
+  geom_line() +
+  theme_light() +
+  scale_x_log10(labels = labels, breaks = breaks) +
+  scale_y_log10(labels = labels, breaks = breaks) +
+  labs(x="Miss Distance (km)", y="Cumulative Number of Encounters", color="Cluster",
+       title="Cumulative Number of Encounters by Cluster", subtitle = paste0("Encounters from 20OCT2019-", today))+
+  scale_color_brewer(palette="Set1")
+
+ggsave(paste0("output/cumulativeEncounters_", today, ".jpeg"),device="jpeg")
+######################################3
+######################
+# WORST OFFENDERS
+# fit persistence predictor model
+alts = c(775,850,975,1500)
+pers = c(300,500,1800,20000)
+persistence = cbind(alts, pers) %>% as_tibble()
+model = lm(log(pers) ~ alts, persistence)
+intercept = as.numeric(model$coefficients[1])
+slope = as.numeric(model$coefficients[2])
+exp(intercept + slope*350) # check value
+
+# get operational satellites
+opSats = derelictDat %>% filter(avgAlt < 2000 & operational) # df of operational sats
+
+# calculate combined mass, persistence, and op sats for each conjunction
+combinedMass_v = vector()
+persistence_v = vector()
+numOpSats_v = vector()
+for (i in 1:nrow(all_conjs)){
+  conj = all_conjs[i,]
+  noradId1 = gsub("--.*", "", conj$PrimarySatellite)
+  noradId2 = gsub("--.*", "", conj$SecondarySatellite)
+  obj1 = filter(mcma_objs, noradId == noradId1)
+  obj2 = filter(mcma_objs, noradId == noradId2)
+  
+  combinedMass = as.numeric(obj1$mass) + as.numeric(obj2$mass)
+  persistence = exp(intercept + slope*conj$Altitude)
+  
+  numOpSats = 0
+  for (j in 1:nrow(opSats)){ # count how many op sats overlap in altitude
+    opSat = opSats[j, ]
+    conjMinAlt = conj$Altitude - 100
+    conjMaxAlt = conj$Altitude + 100
+    if ( ( opSat$perigee > conjMinAlt & opSat$apogee < conjMaxAlt ) ||
+         ( opSat$perigee < conjMaxAlt & opSat$apogee > conjMaxAlt ) ||
+         ( opSat$perigee < conjMinAlt & opSat$apogee > conjMinAlt ) ){
+      numOpSats = numOpSats + 1
+    }
+  }
+  
+  combinedMass_v = append(combinedMass_v, toString(combinedMass))
+  persistence_v = append(persistence_v, persistence)
+  numOpSats_v = append(numOpSats_v, numOpSats)
+}
+all_conjs$combinedMass = combinedMass_v
+all_conjs$persistence = persistence_v
+all_conjs$numOpSats = numOpSats_v
+
+all_conjs = all_conjs %>% 
+  mutate(combinedMass = if_else(grepl(",", combinedMass, fixed=T), # if it contains a comma
+                                as.numeric(gsub(",.*", "", combinedMass)), # make substring up to the comma
+                                as.numeric(combinedMass))) %>% # otherwise don't change
+  mutate(risk = (combinedMass*persistence*numOpSats) / Range)
+
+# sum risk for each object:
+# list all ocnjunctions by first sat, then by second sat, then bind by rows
+firstSet = all_conjs %>% 
+  mutate(noradId = as.numeric(gsub("--.*", "", PrimarySatellite))) %>%
+  dplyr::select(-c(PrimarySatellite, SecondarySatellite))
+secondSet = all_conjs %>% 
+  mutate(noradId = as.numeric(gsub("--.*", "", SecondarySatellite))) %>%
+  dplyr::select(-c(PrimarySatellite, SecondarySatellite))
+
+all_conjs_expanded = rbind(firstSet, secondSet)
+
+mcma_objs = all_conjs_expanded %>%
+  group_by(noradId) %>% 
+  summarise(totalRisk = sum(risk)) %>%
+  right_join(mcma_objs, by="noradId") %>% 
+  mutate(totalRisk = if_else(is.na(totalRisk), 0, totalRisk))
+
+mcma_objs %>% 
+  arrange(desc(totalRisk)) %>%
+  group_by(cluster_new) %>%
+  summarise(p = n()) %>% 
+  mutate(p = p/nrow(mcma_objs)*100,
+         cluster_new = factor(cluster_new,
+                             levels = c("cc615", "cc775", "cc850", "cc975", "cc1200", "cc1500", "cleo","CHIGH"),
+                             ordered = T))%>%
+  ggplot(aes(x=1, y=p, fill=cluster_new)) +
+  geom_col(width = .5)+
+  geom_text(position = position_stack(vjust=.5), 
+            aes(label=paste0(round(p), "%")))+
+  theme_minimal()+
+  theme(axis.title.x = element_blank(), axis.text = element_blank(), 
+        panel.grid.major = element_blank(), panel.grid.minor = element_blank())+
+  labs(fill="Cluster", title="'Risk' per cluster",
+       y="Risk")
+
+ggsave(paste0("output/riskPerCluster_", today, ".jpeg"),device="jpeg")
+
+mcma_objs %>% 
+  arrange(desc(totalRisk)) %>%
+  head(100) %>% View()
+
+  rowid_to_column() %>%
+  ggplot(aes(x=totalRisk)) +
+  geom_histogram(bins=100)
+
+#########################################
+
 # PC FOR CONJUNCTIONS
 #obj1 = data.frame(noradId = 123, mass=1434, length=11, diam =3.9)
 #obj2 = data.frame(noradId = 456, mass=700, length=4, diam = 4)
@@ -131,90 +260,10 @@ all_conjs %>% group_by(cluster) %>%
             CR_wang = sum(pc_wang),
             CR_knowles = sum(pc_knowles))
 
-######################
-# WORST OFFENDERS
-# fit persistence predictor model
-alts = c(775,850,975,1500)
-pers = c(300,500,1800,20000)
-persistence = cbind(alts, pers) %>% as_tibble()
-model = lm(log(pers) ~ alts, persistence)
-exp(1.535505 + 0.005653*350)
-
-derelictDat = readRDS("derelictDatNew")
-opSats = derelictDat %>% filter(avgAlt < 2000 & operational) # df of operational sats
-
-# calculate combined mass, persistence, and op sats for each conjunction
-combinedMass_v = vector()
-persistence_v = vector()
-numOpSats_v = vector()
-for (i in 1:nrow(all_conjs)){
-  conj = all_conjs[i,]
-  noradId1 = gsub("--.*", "", conj$PrimarySatellite)
-  noradId2 = gsub("--.*", "", conj$SecondarySatellite)
-  obj1 = filter(mcma_objs, noradId == noradId1)
-  obj2 = filter(mcma_objs, noradId == noradId2)
-  
-  combinedMass = as.numeric(obj1$mass) + as.numeric(obj2$mass)
-  persistence = exp(1.535505 + 0.005653*conj$Altitude)
-  numOpSats = 0
-  for (j in 1:nrow(opSats)){ # count how many op sats overlap in altitude
-    opSat = opSats[j, ]
-    if (conj$Altitude > opSat$perigee & conj$Altitude < opSat$apogee){
-      numOpSats = numOpSats + 1
-    }
-  }
-  
-  combinedMass_v = append(combinedMass_v, combinedMass)
-  persistence_v = append(persistence_v, persistence)
-  numOpSats_v = append(numOpSats_v, numOpSats)
-}
-all_conjs$combinedMass = combinedMass_v
-all_conjs$persistence = persistence_v
-all_conjs$numOpSats = numOpSats_v
-
-# now total for each object
-for (i in 1:nrow(all_conjs)){
-  conj = all_conjs[i,]
-  noradId1 = gsub("--.*", "", conj$PrimarySatellite)
-  noradId2 = gsub("--.*", "", conj$SecondarySatellite)
-  obj1 = filter(mcma_objs, noradId == noradId1)
-  obj2 = filter(mcma_objs, noradId == noradId2)
-}
-
-# for probability, need cumulative PC and frequency of interaction for each obj
-all_conjs %>% mutate(sat1 = gsub("--.*", "", PrimarySatellite),
-                     sat2 = gsub("--.*", "", SecondarySatellite))
-
-
 #######################
 # number of encounters per cluster pairing
 all_conjs %>% group_by(clusterLab) %>%
   summarise(`number of encounters` = n())
-
-# axis ticks for log scale
-ticks <- 1:10
-ooms <- 10^(0:3) # define the orders of magnitudes
-breaks <- as.vector(ticks %o% ooms)
-
-show.labels <- c(T, F, F, F, T, F, T, F, F)
-labels <- as.character(breaks * show.labels)
-labels <- gsub("^0$", "", labels)
-
-# miss distance vs cumulative count plot
-all_conjs %>%
-  group_by(clusterLab) %>%
-  arrange(Range) %>%
-  mutate(rowid = 1, cumnum = cumsum(rowid)) %>% 
-  ggplot(aes(x=Range, y = cumnum, color=clusterLab)) + 
-  geom_line() +
-  theme_light() +
-  scale_x_log10(labels = labels, breaks = breaks) +
-  scale_y_log10(labels = labels, breaks = breaks) +
-  labs(x="Miss Distance (km)", y="Cumulative Number of Encounters", color="Cluster",
-       title="Cumulative Number of Encounters by Cluster", subtitle = paste0("Encounters from 20OCT2019-", today))+
-  scale_color_brewer(palette="Set1")
-
-ggsave(paste0("/Users/rachelwitner/Documents/Centauri/fall part time/plots/cumulativeEncounters_", today, ".jpeg"),device="jpeg")
 
 ######################
 all_conjs %>% group_by(clusterLab) %>% 
